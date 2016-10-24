fdc5e015eaea319b3067eb9e9fc115a3e800c93c
Amir Ayub <dev.amirayub@gmail.com>
2016-10-24 19:16:36 +0400

    Added data incrementation

--- app/src/main/java/com/aaxs/nitrov17/timestable/MainActivity.java
+++ app/src/main/java/com/aaxs/nitrov17/timestable/MainActivity.java
@@ -2,6 +2,7 @@ package com.aaxs.nitrov17.timestable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
@@ -11,9 +12,11 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     SeekBar seekBar;
-    ArrayList<Float> arrayList;
-    ArrayAdapter<Float> arrayAdapter;
+    ArrayList<Integer> arrayList;
+    ArrayAdapter<Integer> arrayAdapter;
     ListView listView;
+    int seekBarMax = 19;
+    int seekBarMin = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +25,60 @@ public class MainActivity extends AppCompatActivity {
 
         seekBar=(SeekBar)findViewById(R.id.seekBar);
         listView = (ListView)findViewById(R.id.listView);
-        arrayList = new ArrayList<>();
-        populateArr(arrayList);
-        setAdapter();
-    }
 
-    ArrayList populateArr(ArrayList arrayList)
+        setSeekBar();
+        initList();
+        setAdapter();
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                int a = i+1;
+                //Log.d("Seekbar val",String.valueOf(a));
+                updateList(a);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+    void initList()
     {
+        arrayList = new ArrayList<>();
         for(int i=0;i<10;i++)
         {
-            arrayList.add((float)i+1);
+            arrayList.add((int)i+1);
         }
-        return arrayList;
     }
 
     void setAdapter()
     {
-        arrayAdapter = new ArrayAdapter<Float>(this,android.R.layout.simple_list_item_1,arrayList);
+        arrayAdapter = new ArrayAdapter<Integer>(this,android.R.layout.simple_list_item_1,arrayList);
         listView.setAdapter(arrayAdapter);
+    }
+
+    void setSeekBar()
+    {
+        seekBar.setMax(seekBarMax);
+    }
+
+    void updateList(int mux)
+    {
+        listView.invalidateViews();
+        for(int i=0;i<10;i++)
+        {
+            int a = arrayList.get(i);
+            a*=mux;
+            arrayList.set(i,a);
+        }
     }
 
 }

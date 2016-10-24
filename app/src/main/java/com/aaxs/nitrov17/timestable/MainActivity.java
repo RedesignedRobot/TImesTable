470828426622788683fdb2808c6014149a34cb7c
Amir Ayub <dev.amirayub@gmail.com>
2016-10-24 19:46:08 +0400

    Core functionality added

--- app/src/main/java/com/aaxs/nitrov17/timestable/MainActivity.java
+++ app/src/main/java/com/aaxs/nitrov17/timestable/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     SeekBar seekBar;
     ArrayList<Integer> arrayList = new ArrayList<>();
     ArrayAdapter<Integer> arrayAdapter;
+    TextView textView;
     ListView listView;
     int seekBarMax = 19;
     int seekBarMin = 1;
@@ -25,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
         seekBar=(SeekBar)findViewById(R.id.seekBar);
         listView = (ListView)findViewById(R.id.listView);
-
+        textView =(TextView)findViewById(R.id.textView);
+        textView.setText("Try the slider out!");
         setSeekBar();
         initList();
         setAdapter();
@@ -37,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 //deInitList();
                 //initList();
                 int a = i+1;
+                textView.setText(String.valueOf(i+1)+"'s Time Table");
                 //Log.d("Seekbar val",String.valueOf(a));
                 updateList(a);
             }
